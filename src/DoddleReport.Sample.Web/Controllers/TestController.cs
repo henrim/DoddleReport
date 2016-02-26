dc79e0007bdb0e0123397b9936e701aa221eca9f
Henri Merkesdal <hm@firstpoint.no>
2016-02-26 19:26:12 +0100

    vs2015

---
+++ src/DoddleReport.Sample.Web/Controllers/TestController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DoddleReport.Sample.Web.Models;
+using DoddleReport.Web;
+using DoddleReport.Writers;
+
+namespace DoddleReport.Sample.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public ReportResult ListOfObjects()
+        {
+            var list = new List<object>();
+
+
+            list.Add(new
+                         {
+                             FirstName = "Matt",
+                             LastName = "Hidinger"
+                         });
+
+            var report = new Report(list.ToReportSource());
+            return new ReportResult(report);
+        }
+
+        public ReportResult WithExtension()
+        {
+            var query = DoddleProductRepository.GetAll();
+            var report = new Report(query.ToReportSource());
+            return new ReportResult(report) { FileName = "CustomName.html"};
+        }
+
+        public ReportResult CustomHtmlStyle()
+        {
+            var query = DoddleProductRepository.GetAll();
+            var report = new Report(query.ToReportSource());
+
+            //report.RenderHints["HtmlStyle"] = @".htmlReport th { font-size: 40px !important; }";
+            foreach (var dataField in report.DataFields)
+            {
+                dataField.HeaderStyle.FontSize = 13;
+            }
+            return new ReportResult(report);
+        }
+
+        public ReportResult WithExtensionAndWriter()
+        {
+            var query = DoddleProductRepository.GetAll();
+            var report = new Report(query.ToReportSource());
+            return new ReportResult(report, new DelimitedTextReportWriter()) { FileName = "CustomName.txt" };
+        }
+
+        public ReportResult WithoutExtension()
+        {
+            var query = DoddleProductRepository.GetAll();
+            var report = new Report(query.ToReportSource());
+            return new ReportResult(report) { FileName = "CustomName" };
+        }
+    }
+}
---
+++ src/DoddleReport.Sample.Web/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+using DoddleReport.Web;
+
+namespace DoddleReport.Sample.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //DelimitedTextReportWriter.GetHeaderText = field => field.HeaderText;
+
+
+            routes.MapRoute("LegacyUrl",
+                    "home/{action}.{extension}",
+                    new { controller = "Doddle" },
+                    new { extension = new ReportRouteConstraint() }
+                );
+
+            routes.MapReportingRoute();
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional, area = "" } // Parameter defaults
+            );
+        }
+
+        protected void Application_Start()
+        {
+            TryRegisterAbcPdf();
+            
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static void TryRegisterAbcPdf()
+        {
+            try
+            {
+                var abcPdfLicenseKey = ConfigurationManager.AppSettings["AbcPdfLicense"];
+
+                if (!string.IsNullOrWhiteSpace(abcPdfLicenseKey))
+                {
+                    var success = WebSupergoo.ABCpdf7.XSettings.InstallRedistributionLicense(abcPdfLicenseKey);
+                    Debug.WriteLine("ABC PDF Registered: {0}", success);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
